d4044b1236ba384c86d7f575e3492705f247572a
Souza <joao.souza@capgemini.com>
2022-09-01 10:19:23 -0300

    Adicionado Swagger

--- src/config/server.ts
+++ src/config/server.ts
@@ -11,7 +11,7 @@ const app = express()
 // Set Middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors({ origin: process.env.CROSS_ORIGIN }))
 app.use(morgan('dev'))
 app.use('/', routes)
 app.use(BugLaucher)
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,12 @@
-import { Router } from 'express'
+import { Request, Response, Router } from 'express'
 
 const routes = Router()
+
+routes.get('/', (req: Request, res: Response) => {
+  return res.status(200).send({
+    message: 'Hello! This is ok',
+    port: process.env.PORT
+  })
+})
 
 export { routes }
